b11bb7e5a2fa96cdf29d2f85bb4539ea3dbbb85b
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-08-09 08:51:38 +0200

    Challenge 3 (improv)

--- api/src/routes/utils/arrayify.js
+++ api/src/routes/utils/arrayify.js
@@ -2,9 +2,6 @@ export default arrayify
 // Here's where you'll write arrayify. Look at
 // `api/src/routes/utils/arrayify.js` for instructions.
 
-function arrayify(arg) {
-  if (arg) {
-    return Array.isArray(arg) ? arg : [arg]
-  }
-  return []
+function arrayify(arg = []) {
+  return Array.isArray(arg) ? arg : [arg]
 }
