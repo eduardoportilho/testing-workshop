b0b91dee08d0a788e21fad5e2df0b7e5a1f072ce
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-08-11 08:56:11 +0200

    Integration test demo

--- api/demo/integration/__tests__/users.js
+++ api/demo/integration/__tests__/users.js
@@ -1,1 +1,22 @@
-test('todo', () => {})
+import axios from 'axios'
+import startServer from '../start-server'
+
+let server
+
+beforeAll(() => {
+  return startServer().then(s => {
+    server = s
+  })
+})
+
+afterAll(done => {
+  server.close(done)
+})
+
+test('can get users', () => {
+  axios
+    .get('http://localhost:3001/api/users')
+    .then(respose => {
+      console.log(response.data.users)
+    })
+})
